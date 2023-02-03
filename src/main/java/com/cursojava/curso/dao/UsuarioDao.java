c3656efd54402e9362e294ce35f4c5f287c5184e
Steven Calderon <steven_cratus@hotmail.com>
2023-02-03 02:04:11 -0500

    Implements BD

---
+++ src/main/java/com/cursojava/curso/dao/UsuarioDao.java
@@ -0,0 +1,11 @@
+package com.cursojava.curso.dao;
+
+import com.cursojava.curso.models.Usuario;
+
+import java.util.List;
+
+// Interfaces? Son un contrato que obliga a los que usen a implementar las funciones
+public interface UsuarioDao {
+    List<Usuario> getUsuarios();
+
+}
--- src/main/resources/static/js/usuarios.js
+++ src/main/resources/static/js/usuarios.js
@@ -6,15 +6,26 @@ cargarUsuarios();
 
 async function cargarUsuarios (){
 
-  const request = await fetch('usuarios/231', {
+  const request = await fetch('usuarios', {
     method: 'GET',
     headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json'
     }
   });
-  const usuarios = await request.json();
 
+    const usuarios = await request.json();
+    let rowsUser ="";
+
+  for(let usuario of usuarios){
+    let rowUser = `<tr> <td>${usuario.id}</td> <td>${usuario.nombre}
+    ${usuario.apellido}</td> <td>${usuario.email}</td> <td>${usuario.telefono}</td>
+    <td> <a href="#" class="btn btn-danger btn-circle btn-sm"> <i class="fas fa-trash"></i>
+    </a> </td> </tr>`;
+    rowsUser +=rowUser;
+
+  }
+  document.querySelector("#usuarios tbody").outerHTML=rowsUser
   console.log(usuarios);
 
 }
